cd27ab93e45911f024a50bdb35d9f04b0ce65c76
Nikolay Savin <info@megawebs.kz>
2018-06-19 13:27:21 +0600

    inital

---
+++ ajaxForm.js
@@ -0,0 +1,168 @@
+var AjaxForm = {
+
+    initialize: function (afConfig) {
+        if (!jQuery().ajaxForm) {
+            document.write('<script src="' + afConfig['assetsUrl'] + 'js/lib/jquery.form.min.js"><\/script>');
+        }
+        if (!jQuery().jGrowl) {
+            document.write('<script src="' + afConfig['assetsUrl'] + 'js/lib/jquery.jgrowl.min.js"><\/script>');
+        }
+
+        $(document).ready(function () {
+            $.jGrowl.defaults.closerTemplate = '<div>[ ' + afConfig['closeMessage'] + ' ]</div>';
+        });
+
+        $(document).off('submit', afConfig['formSelector']).on('submit', afConfig['formSelector'], function (e) {
+            $(this).ajaxSubmit({
+                dataType: 'json',
+                data: {pageId: afConfig['pageId']},
+                url: afConfig['actionUrl'],
+                beforeSerialize: function (form) {
+                    form.find(':submit').each(function () {
+                        if (!form.find('input[type="hidden"][name="' + $(this).attr('name') + '"]').length) {
+                            $(form).append(
+                                $('<input type="hidden">').attr({
+                                    name: $(this).attr('name'),
+                                    value: $(this).attr('value')
+                                })
+                            );
+                        }
+                    })
+                },
+                beforeSubmit: function (fields, form) {
+                    //noinspection JSUnresolvedVariable
+                    if (typeof(afValidated) != 'undefined' && afValidated == false) {
+                        return false;
+                    }
+                    form.find('.error').html('');
+                    form.find('.error').removeClass('error');
+                    form.find('input,textarea,select,button').attr('disabled', true);
+                    return true;
+                },
+                success: function (response, status, xhr, form) {
+                    form.find('input,textarea,select,button').attr('disabled', false);
+                    response.form = form;
+                    $(document).trigger('af_complete', response);
+                    if (!response.success) {
+                        //AjaxForm.Message.error(response.message);
+                        var $type = 'error';
+                        var $message = response.message;
+                        var $icon = 'fa fa-exclamation-circle';
+                        toastOptions = {
+                            class: 'iziToast-' + $type,
+                            message: $message,
+                            animateInside: false,
+                            position: 'topRight',
+                            progressBar: false,
+                            icon: $icon,
+                            timeout: 3200,
+                            transitionIn: 'fadeInLeft',
+                            transitionOut: 'fadeOut',
+                            transitionInMobile: 'fadeIn',
+                            transitionOutMobile: 'fadeOut',
+                            theme:'dark'
+                        };
+                        iziToast.show(toastOptions);
+                        if (response.data) {
+                            var key, value, focused;
+                            for (key in response.data) {
+                                if (response.data.hasOwnProperty(key)) {
+                                    if (!focused) {
+                                        form.find('[name="' + key + '"]').focus();
+                                        focused = true;
+                                    }
+                                    value = response.data[key];
+                                    form.find('.error_' + key).html(value).addClass('error');
+                                    form.find('[name="' + key + '"]').addClass('error');
+                                }
+                            }
+                        }
+                    }
+                    else {
+                        //AjaxForm.Message.success(response.message);
+                        var $type = 'success';
+                        var $message = response.message;
+                        var $icon = 'fa fa-check';
+                        toastOptions = {
+                            class: 'iziToast-' + $type,
+                            message: $message,
+                            animateInside: false,
+                            position: 'topRight',
+                            progressBar: false,
+                            icon: $icon,
+                            timeout: 3200,
+                            transitionIn: 'fadeInLeft',
+                            transitionOut: 'fadeOut',
+                            transitionInMobile: 'fadeIn',
+                            transitionOutMobile: 'fadeOut',
+                            theme:'dark'
+                        };
+                        iziToast.show(toastOptions);
+                        form.find('.error').removeClass('error');
+                        form[0].reset();
+                        //noinspection JSUnresolvedVariable
+                        if (typeof(grecaptcha) != 'undefined') {
+                            //noinspection JSUnresolvedVariable
+                            grecaptcha.reset();
+                        }
+                    }
+                }
+            });
+            e.preventDefault();
+            return false;
+        });
+
+        $(document).on('keypress change', '.error', function () {
+            var key = $(this).attr('name');
+            $(this).removeClass('error');
+            $('.error_' + key).html('').removeClass('error');
+        });
+
+        $(document).on('reset', afConfig['formSelector'], function () {
+            $(this).find('.error').html('');
+            AjaxForm.Message.close();
+        });
+    }
+
+};
+
+
+//noinspection JSUnusedGlobalSymbols
+AjaxForm.Message = {
+    success: function (message, sticky) {
+        if (message) {
+            if (!sticky) {
+                sticky = false;
+            }
+            $.jGrowl(message, {theme: 'af-message-success', sticky: sticky});
+        }
+    },
+    error: function (message, sticky) {
+        if (message) {
+            if (!sticky) {
+                sticky = false;
+            }
+            $.jGrowl(message, {theme: 'af-message-error', sticky: sticky});
+        }
+    },
+    info: function (message, sticky) {
+        if (message) {
+            if (!sticky) {
+                sticky = false;
+            }
+            $.jGrowl(message, {theme: 'af-message-info', sticky: sticky});
+        }
+    },
+    close: function () {
+        $.jGrowl('close');
+    },
+};
+
+$(document).on('af_complete', function(event, response) {
+    var form = response.form;
+    // Если у формы определённый id
+    if (form.attr('id') == 'callbackForm') {
+        // Скрываем её!
+        form.closest('.modal').modal('hide');
+    }
+});
